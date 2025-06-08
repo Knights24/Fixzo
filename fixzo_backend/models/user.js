2a8e7983f165bd0b50f443e4255b48f205ee7d51
Knights24 <vivekvishwakarma21@outlook.com>
2025-06-08 16:20:27 +0530

    change in original file

--- fixzo_backend/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  phone: String,
-  password: String,
-  role: { type: String, enum: ['customer', 'vendor', 'admin'], default: 'customer' }
-});
-module.exports = mongoose.model('User', userSchema);
